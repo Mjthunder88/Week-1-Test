a40fe8f9f91b8c680693be9d787e5db494f3685d
Marcus Johnson <marcusjohnson88.mj@gmail.com>
2022-09-30 10:12:50 -0600

    Finished-section-1-sdlc

--- sdlc.js
+++ sdlc.js
@@ -19,3 +19,16 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+
+const analysisOfRequirments = "- The process of planning what the user might require/need within the product that is produced. ";
+const design = "- What you want your project to look like and how it should function.";
+const implementation = "The process on how you will implement the design and previous planning of the project.";
+const testingAndIntegration = "Checking for bugs in your code and making sure everything runs properly. As well as integrating new code that may fix or add to your current design.";
+const maintain = "continued maintence on your project to make sure things don't break or ruin the Ux";
+
+// console.log(planning);
+// console.log(analysisOfRequirments);
+// console.log(design);
+// console.log(implementation);
+// console.log(testingAndIntegration);
+// console.log(maintain)
