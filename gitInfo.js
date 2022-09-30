ceee80943574094111cc1330b884151d5a441b7e
Marcus Johnson <marcusjohnson88.mj@gmail.com>
2022-09-30 10:58:46 -0600

    Finished-git-section

--- gitInfo.js
+++ gitInfo.js
@@ -10,7 +10,8 @@
 */
 
 //CODE HERE
-
+let gitDefinition = 'A version control system that helps you send your code/repositorys to GitHub. It also helps you track your progress while coding.';
+// console.log(gitDefinition);
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable called 'gitHubDefinition'.  
@@ -18,7 +19,8 @@
 */
 
 //CODE HERE
-
+let gitHubDefinition = 'A cloud/website service that helps you track your repositorys and upload/download them. It\'s also where you can collaborate with other coders or maybe even your coworkers. ';
+// console.log(gitHubDefinition)
 //////////////////PROBLEM 3////////////////////
 /*
     Create a variable called 'gitInitDefinition'.  
@@ -26,7 +28,8 @@
 */
 
 //CODE HERE
-
+let gitInitDefinition = 'Helps you create a git for the current folder you are in. (Helps you create a repository on your local machine.'
+// console.log(gitInitDefinition);
 //////////////////PROBLEM 4////////////////////
 /*
     Create a variable called 'gitCloneDefinition'.  
@@ -34,7 +37,8 @@
 */
 
 //CODE HERE
-
+let gitCloneDefinition = 'Helps you copy a repository from GitHub and put it on your local machine';
+// console.log(gitCloneDefinition);
 //////////////////PROBLEM 5////////////////////
 /*
     Create a variable called 'gitStatusDefinition'.  
@@ -42,7 +46,8 @@
 */
 
 //CODE HERE
-
+let gitStatusDefinition = 'Helps you see the status of your files in your current repository. It will indicate if something has changed within the files, or if its staged to be sent to GitHub.';
+// console.log(gitStatusDefinition);
 //////////////////PROBLEM 6////////////////////
 /*
     Create a variable called 'gitAddDefinition'.  
@@ -53,7 +58,10 @@
 */
 
 //CODE HERE
-
+let gitAddDefinition = 'Stages your selected files, so that they can be commited/sent to GitHub.';
+let gitAddCode = 'git add .'
+// console.log(gitAddDefinition);
+// console.log(gitAddCode);
 //////////////////PROBLEM 7////////////////////
 /*
     Create a variable called 'gitCommitDefinition'.  
@@ -64,7 +72,10 @@
 */
 
 //CODE HERE
-
+let gitCommitDefinition = 'Creates a snapshot of the  current files that were staged, so  that it can be pushed to  GitHub.';
+let gitCommitCode = 'git commit -m"message of some sort"';
+// console.log(gitCommitDefinition);
+// console.log(gitCommitCode);
 
 //////////////////PROBLEM 8////////////////////
 /*
@@ -73,3 +84,6 @@
 */
 
 //CODE HERE
+
+let  gitPushDefinition = 'Sends your local commits to GitHub/cloud. Anything not commited will not be sent.';
+// console.log(gitPushDefinition);
